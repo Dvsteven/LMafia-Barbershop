e413a079320e8c3f572eab82bba2ff06d6bd4ac1
Dvsteven <stevenhenao81@gmail.com>
2026-07-17 14:16:37 -0500

    Fix: instalación PWA real

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,28 @@
+const CACHE_NAME = 'la-mafia-v2';
+
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('v1').then(cache => {
+    caches.open(CACHE_NAME).then(cache => {
       return cache.addAll([
         '/',
         '/index.html',
-        '/favicon.png'
+        '/manifest.json',
+        '/assets/images/icon-192.png',
+        '/assets/images/icon-512.png',
+        '/assets/images/icon-maskable-512.png'
       ]);
     })
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
